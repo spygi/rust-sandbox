2b560492f8ede8662a3b53abd58ccdbbad212430
Spyros Giannakakis <spgianna@microsoft.com>
2022-03-18 16:13:43 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use rand::prelude::*;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let a = rng.gen_range(0..10);
+    let b = rng.gen_range(0..10);
+
+    println!("Sum of {} and {} is {}", a, b, add(a, b));
+}
+
+fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[test]
+fn test_add() {
+    assert_eq!(3, add(1, 2));
+}
