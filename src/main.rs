d0a54a106f9a9f6ecc45fbdd446cb894c787d177
Spyros Giannakakis <spgianna@microsoft.com>
2022-03-21 20:42:20 +0000

    add submodule, test if action works without unconfined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod util; // bring module util in file util.rs in scope
+
 use rand::prelude::*;
 
 #[cfg(not(tarpaulin_include))]
@@ -6,6 +8,8 @@ fn main() {
     let a = rng.gen_range(0..10);
     let b = rng.gen_range(0..10);
 
+    println!("{}", util::pretty_print(a));
+    println!("{}", util::pretty_print(b));
     println!("Sum of {} and {} is {}", a, b, add(a, b));
 }
 
@@ -13,8 +17,13 @@ fn add(a: u32, b: u32) -> u32 {
     a + b
 }
 
-#[cfg(not(tarpaulin_include))]
-#[test]
-fn test_add() {
-    assert_eq!(3, add(1, 2));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // #[cfg_attr(tarpaulin, ignore)]
+    fn test_add() {
+        assert_eq!(3, add(1, 2));
+    }
 }
---
+++ src/util.rs
@@ -0,0 +1,3 @@
+pub fn pretty_print(a: u32) -> String {
+    format!("number is {}", a)
+}
