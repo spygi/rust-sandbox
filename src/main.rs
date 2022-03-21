15faf6289c174cd687d3bb8439a4942d0276213c
Spyros Giannakakis <spgianna@microsoft.com>
2022-03-21 09:21:27 +0000

    Adds code coverage tool in gh action

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::prelude::*;
 
+#[cfg(not(tarpaulin_include))]
 fn main() {
     let mut rng = rand::thread_rng();
     let a = rng.gen_range(0..10);
@@ -12,7 +13,8 @@ fn add(a: u32, b: u32) -> u32 {
     a + b
 }
 
+#[cfg(not(tarpaulin_include))]
 #[test]
 fn test_add() {
-    assert_eq!(3, add(1, 0));
+    assert_eq!(3, add(1, 2));
 }
